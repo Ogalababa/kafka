cf21edaafd17924ef77aeb6ce1751b0aecd87064
Ogalababa <delay7.su@gmail.com>
2025-04-16 13:10:24 +0200

    frontend add tailwindcss

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App'; // 引入 App 组件
-import './index.css';    // 可选样式
+import './output.css';    // 可选样式
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);

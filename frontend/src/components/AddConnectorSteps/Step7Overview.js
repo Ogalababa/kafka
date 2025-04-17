9008b425d50133e544e12c6dd64231bc94ebf8cb
Ogalababa <delay7.su@gmail.com>
2025-04-17 13:23:22 +0200

    frontend stap7

--- frontend/src/components/AddConnectorSteps/Step7Overview.js
+++ frontend/src/components/AddConnectorSteps/Step7Overview.js
@@ -66,10 +66,7 @@ const Step7Overview = ({ formData, prevStep, closeModal }) => {
 
                 {response ? (
                     <button
-                        onClick={() => {
-                            console.log('✅ Finish clicked');
-                            closeModal();
-                        }}
+                        onClick={() => window.location.reload()}
                         className="bg-green-600 text-white px-4 py-2 rounded hover:bg-green-700"
                     >
                         ✅ Finish
